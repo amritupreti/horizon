b1a8643eb67a4b601835a77b6a4ab0d0361043f6
amritupreti <amritkumarupreti@gmail.com>
2023-07-28 18:22:04 +0545

    Add local keystone url.

--- openstack_auth/defaults.py
+++ openstack_auth/defaults.py
@@ -23,7 +23,7 @@ WEBROOT = '/'
 # TODO(amotoki): What is the right default value in openstack_auth?
 LOGIN_ERROR = 'error/'
 
-OPENSTACK_KEYSTONE_URL = "http://localhost/identity/v3"
+OPENSTACK_KEYSTONE_URL = "http://192.168.56.6:5000"
 
 # TODO(amotoki): The default value in openstack_dashboard is different:
 # publicURL. It should be consistent.
